ebbbccee07c3c1eb40e20cf275cdff6b93417bb0
mj-saini <manojsaini97285@gmail.com>
2025-01-21 10:41:46 +0530

    trying to change theme

---
+++ assets/js/theme.js
@@ -0,0 +1,11 @@
+
+
+const themeToggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggleBtn.addEventListener('click', () => {
+  body.classList.toggle('light_theme'); 
+  themeToggleBtn.textContent = body.classList.contains('light_theme')
+    ? 'Switch to Light Theme'
+    : 'Switch to Dark Theme';
+});
