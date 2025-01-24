740f4621b3b554c921a495b3dcbc4f239ecd0f36
harish joshi <harishjoshi021120004@gmail.com>
2025-01-24 10:44:29 +0530

    theme change

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,30 +1,15 @@
-
-
-// const themeToggleBtn = document.getElementById('theme-toggle');
-// const body = document.body;
-
-// themeToggleBtn.addEventListener('click', () => {
-//   body.classList.toggle('light_theme'); 
-//   themeToggleBtn.textContent = body.classList.contains('light_theme')
-//     ? 'Switch to Light Theme'
-//     : 'Switch to Dark Theme';
-// });
-
-
-
-
-const themeToggleBtn = document.getElementById('theme-toggle');
-const lightIcon = document.getElementById('light-icon');
-const darkIcon = document.getElementById('dark-icon');
+const lightThemeBtn = document.getElementById("light-theme-btn");
+const darkThemeBtn = document.getElementById("dark-theme-btn");
 const body = document.body;
 
-themeToggleBtn.addEventListener('click', () => {
-  body.classList.toggle('light_theme');
-  if (body.classList.contains('light_theme')) {
-    lightIcon.style.display = 'inline-block'; 
-    darkIcon.style.display = 'inline';  
-  } else {
-    lightIcon.style.display = 'inline';
-    darkIcon.style.display = 'none'; 
-  }
+lightThemeBtn.addEventListener("click", () => {
+  body.classList.add("light_theme");
+  lightThemeBtn.classList.add("hidden");
+  darkThemeBtn.classList.remove("hidden");
+});
+
+darkThemeBtn.addEventListener("click", () => {
+  body.classList.remove("light_theme");
+  darkThemeBtn.classList.add("hidden");
+  lightThemeBtn.classList.remove("hidden");
 });
